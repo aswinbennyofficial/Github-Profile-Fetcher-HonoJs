35b2fd62332411953243973d53ed09a5cd5e4683
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-10-14 18:33:56 +0530

    Profile details displayed as html

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ app.get('/', (c) => c.text('Hello Hono!'))
 app.get('/github/:id', async (c) =>{
     const id=c.req.param("id");
     const Url='https://api.github.com/users/'+id;
-    //let jsonOut=JSON.parse(Url);
+    
 
     const fetched = await fetch(Url);
     const profileDetails = await fetched.json();
     console.log(profileDetails);
 
     
-   return c.json(profileDetails);
-}
+   return c.html(`<h1>${profileDetails.name}<h1>`);
+} 
 );
 
 app.use('/static/*', serveStatic({ root: './' }))
