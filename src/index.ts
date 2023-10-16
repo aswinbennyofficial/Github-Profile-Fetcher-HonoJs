6fca06dc754dadd4ae75b0f703b68ee746cb8aa8
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-10-16 17:20:51 +0530

    added repolist into the profilejson

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,67 @@ import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
 app.get('/', (c) => c.text('Hello Hono!'))
+// app.get('/github/:id', async (c) =>{
+//     const id=c.req.param("id");
+//     const ProfileUrl='https://api.github.com/users/'+id;
+    
+
+//     const Profilefetched = await fetch(Url);
+//     const profileDetails = await Profilefetched.json();
+//     console.log(profileDetails);
+
+    
+//    return c.json(profileDetails);
+// } 
+// );
+
+
+// app.get('/repos/:id', async (c) =>{
+//     const id=c.req.param("id");
+//     const repoUrl='https://api.github.com/users/'+id+'/repos';
+    
+
+//     const repofetched = await fetch(repoUrl);
+//     const repoJson = await repofetched.json();
+//     console.log(repoJson);
+
+//     const repoarray : string[]=[]; //explicitly type the array;
+
+//     for( var i in repoJson){
+//         repoarray.push(repoJson[i].name);
+//     }
+    
+    
+//    return c.text(repoarray[0]);
+// } 
+// );
+
 app.get('/github/:id', async (c) =>{
     const id=c.req.param("id");
-    const Url='https://api.github.com/users/'+id;
+    const ProfileUrl='https://api.github.com/users/'+id;
+    const repoUrl='https://api.github.com/users/'+id+'/repos';
     
 
-    const fetched = await fetch(Url);
-    const profileDetails = await fetched.json();
-    console.log(profileDetails);
+    // for profile info
+    const Profilefetched = await fetch(ProfileUrl);
+    const profileJson = await Profilefetched.json();
 
-    
-   return c.html(`<h1>${profileDetails.name}<h1>`);
-} 
-);
-
-
-app.get('/repos/:id', async (c) =>{
-    const id=c.req.param("id");
-    const Url='https://api.github.com/users/'+id+'/repos';
-    
-
-    const fetched = await fetch(Url);
-    const repoJson = await fetched.json();
-    console.log(repoJson);
+    //for repo info
+    const repofetched = await fetch(repoUrl);
+    const repoJson = await repofetched.json();
 
     const repoarray : string[]=[]; //explicitly type the array;
 
     for( var i in repoJson){
         repoarray.push(repoJson[i].name);
     }
+
+   
+    const final = { ...profileJson, repo1: repoarray };
+
+    return c.json(final);
+
     
-    
-   return c.text(repoarray[0]);
 } 
 );
 
