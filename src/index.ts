03f0f5779036514c7736212ce034c3261da0e507
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-10-16 15:44:02 +0530

    added repo route and implemented json to array

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,27 @@ app.get('/github/:id', async (c) =>{
 } 
 );
 
+
+app.get('/repos/:id', async (c) =>{
+    const id=c.req.param("id");
+    const Url='https://api.github.com/users/'+id+'/repos';
+    
+
+    const fetched = await fetch(Url);
+    const repoJson = await fetched.json();
+    console.log(repoJson);
+
+    let repoarray=[];
+
+    for( var i in repoJson){
+        repoarray.push(repoJson[i].name);
+    }
+    
+    
+   return c.text(repoarray[0]);
+} 
+);
+
 app.use('/static/*', serveStatic({ root: './' }))
 
 serve(app);
